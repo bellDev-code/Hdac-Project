17cbb9774c788c736310ecaeb7c7fdf57091d7b6
bellDev-code <sumaoo20@naver.com>
2021-08-30 16:44:24 +0900

    React setting

---
+++ front/webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const webpack = require('webpack');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config = {
+  name: 'Hadac-Project',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@Hooks': path.resolve(__dirname, 'src/Hooks'),
+      '@Components': path.resolve(__dirname, 'src/Components'),
+      '@Layouts': path.resolve(__dirname, 'src/Layouts'),
+      '@Pages': path.resolve(__dirname, 'src/Pages'),
+      '@Utils': path.resolve(__dirname, 'src/Utils'),
+      '@Assets': path.resolve(__dirname, 'src/Assets'),
+    },
+  },
+  entry: {
+    app: './src/Client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['last 2 chrome versions'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          env: {
+            development: {
+              plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
+            },
+            production: {
+              plugins: ['@emotion'],
+            },
+          },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' })],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true, // react router
+    port: 3090,
+    publicPath: '/dist/',
+    // proxy: {
+    //   '/api/': {
+    //     target: 'http://localhost:3095',
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+  // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+}
+if (!isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+module.exports = config;
