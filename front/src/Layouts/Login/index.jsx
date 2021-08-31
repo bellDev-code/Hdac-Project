1d97cef055f40c3305d11fbbf2b87acd977237a9
bellDev-code <sumaoo20@naver.com>
2021-08-31 14:46:41 +0900

    Login page and button size components

--- front/src/Layouts/Login/index.jsx
+++ front/src/Layouts/Login/index.jsx
@@ -1,8 +1,33 @@
+import MiddleBtn from '@Components/Button/middleBtn';
 import React from 'react';
-import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Container, AccountForm, LoginForm, ButtonWrapper } from './styles';
 
 const Login = () => {
-  return <Container>Login</Container>;
+  const LoginSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <AccountForm>
+        <h3>Login</h3>
+        <LoginForm onSubmit={LoginSubmit}>
+          <label>username</label>
+          <input type="text" />
+          <label>password</label>
+          <input type="password" />
+          <ButtonWrapper>
+            <MiddleBtn type="submit" />
+          </ButtonWrapper>
+          <Link to="/register" style={{ paddingTop: '20px' }}>
+            Create Your Account?
+          </Link>
+          <Link to="/find">Find Your Username?</Link>
+        </LoginForm>
+      </AccountForm>
+    </Container>
+  );
 };
 
 export default Login;
