c0f21b849dd1c14a7a22584d264d1f9cfa7be6e1
bellDev-code <sumaoo20@naver.com>
2021-09-04 23:16:33 +0900

    Squashed commit of the following:
    commit b66df9f8f127f6fb656ba1934365aef8fec748dc
    Author: bellDev-code <sumaoo20@naver.com>
    Date:   Sat Sep 4 23:15:42 2021 +0900
    
        git test

--- front/src/Components/Home/MusicList/styles.jsx
+++ front/src/Components/Home/MusicList/styles.jsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  padding: 50px 0;
+`;
 
 export const Title = styled.h1`
   font-size: 35px;
