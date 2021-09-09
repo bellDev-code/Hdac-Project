a0f456446730ad23d4e3a7b3241916f1199faa99
bellDev-code <sumaoo20@naver.com>
2021-09-09 11:27:26 +0900

    npm i Material UI MusicList components flex-wrap

--- front/src/Components/Home/MusicCard/index.jsx
+++ front/src/Components/Home/MusicCard/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { CardWrapper, Wrapper, ImageWrapper, CardImage, TextWrapper, CardInfo, CardFunding, CardDay } from './styles';
 
-import AlbumImage from '@Assets/Img/MusicList/music01.jpg';
-import AlbumImage02 from '@Assets/Img/MusicList/music02.jpg';
-import AlbumImage03 from '@Assets/Img/MusicList/music03.jpg';
+import AlbumImage from '@Assets/Img/MusicList/music03.jpg';
 
 const CardData = [
   {
@@ -17,7 +15,7 @@ const CardData = [
   },
   {
     id: 2,
-    url: `${AlbumImage02}`,
+    url: `${AlbumImage}`,
     name: 'NEXT LEVEL',
     artist: 'Aespa(에스파)',
     achieved: '7,000원',
@@ -26,7 +24,34 @@ const CardData = [
   },
   {
     id: 3,
-    url: `${AlbumImage03}`,
+    url: `${AlbumImage}`,
+    name: 'THE ALBUM',
+    artist: 'BlackPink(블랙핑크)',
+    achieved: '10,000,000원',
+    goal: '10,000,000원',
+    exp: '13일',
+  },
+  {
+    id: 4,
+    url: `${AlbumImage}`,
+    name: 'LILAC',
+    artist: 'IU(아이유)',
+    achieved: '3,000원',
+    goal: '8,000,000원',
+    exp: '20일',
+  },
+  {
+    id: 5,
+    url: `${AlbumImage}`,
+    name: 'NEXT LEVEL',
+    artist: 'Aespa(에스파)',
+    achieved: '7,000원',
+    goal: '9,000,000원',
+    exp: '30일',
+  },
+  {
+    id: 6,
+    url: `${AlbumImage}`,
     name: 'THE ALBUM',
     artist: 'BlackPink(블랙핑크)',
     achieved: '10,000,000원',
@@ -46,13 +71,12 @@ const MusicCard = () => {
             </ImageWrapper>
             <TextWrapper>
               <CardInfo>
-                {item.name}
-                {item.artist}
+                {item.name} - {item.artist}
               </CardInfo>
               <CardFunding>
                 {item.achieved}/{item.goal}
               </CardFunding>
-              <CardDay>{item.exp}</CardDay>
+              <CardDay>D-Day {item.exp}</CardDay>
             </TextWrapper>
           </Wrapper>
         );
--- front/src/Components/Home/MusicCard/styles.jsx
+++ front/src/Components/Home/MusicCard/styles.jsx
@@ -2,26 +2,52 @@ import styled from '@emotion/styled';
 
 export const CardWrapper = styled.div`
   display: flex;
-  padding: 15px 0;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 export const Wrapper = styled.div`
+  width: 30%;
   display: flex;
+  border: 1px solid #dee2e6;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f8f9fa;
+  cursor: pointer;
+
+  :hover {
+    background-color: #f1f3f5;
+  }
 `;
 
 export const ImageWrapper = styled.div`
+  width: 50%;
   padding: 0 16px;
-  width: 30%;
+  flex: 1;
 `;
 
 export const CardImage = styled.img`
   width: 100%;
 `;
 
-export const TextWrapper = styled.div``;
+export const TextWrapper = styled.div`
+  width: 50%;
+  text-align: center;
+  flex: 1;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
 
-export const CardInfo = styled.div``;
+export const CardInfo = styled.div`
+  padding: 10px 0;
+`;
 
-export const CardFunding = styled.div``;
+export const CardFunding = styled.div`
+  padding: 10px 0;
+`;
 
-export const CardDay = styled.div``;
+export const CardDay = styled.div`
+  padding: 10px 0;
+`;
