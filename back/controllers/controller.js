044996caed5616ad9c830a32b17b177bbd7d16e2
wkjung0624 <skyship36@gmail.com>
2021-09-15 04:40:49 +0900

    Add send query function to controller

--- back/controllers/controller.js
+++ back/controllers/controller.js
@@ -1,4 +1,5 @@
 const passport = require('../models/passport');
+const mariaDB = require('../models/mariadb')
 
 module.exports = {
     getAuthenticate: passport.authenticate('kakao'),
@@ -24,5 +25,37 @@ module.exports = {
     f2 : (req, res) => { 
         console.log(req.user,3)
         res.send("f2") 
+    },
+
+    getQuery : async (req, res) => {
+        try {
+            const queryResult = await mariaDB.sendQuery(req.params.alias)
+            res.send(JSON.stringify(queryResult))
+        } catch (err) {
+          res.status(500).send({
+            error: err,
+            queryName : req.params.alias,
+            bodyParams : req.body.param,
+            where : req.body.where
+          });
+        } 
+    },
+
+    postQuery : async (req, res) => {
+        try {
+            await mariaDB.sendQuery(req.params.alias);
+            if (req.body.param.length > 0) {
+              for (let key in req.body.param[0]) req.session[key] = req.body.param[0][key];
+              res.send(req.body.param[0]);
+            } else {
+              res.send({
+                error: "Please try again or contact system manager."
+              });
+            }
+          } catch (err) {
+            res.send({
+              error: "DB access error"
+            });
+          }
     },
 }
