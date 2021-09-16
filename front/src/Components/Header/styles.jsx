ed1634c9bf84c65334bb2ff68671b58820c10405
bellDev-code <sumaoo20@naver.com>
2021-09-16 16:52:49 +0900

    Add Fonts "Jua"

--- front/src/Components/Header/styles.jsx
+++ front/src/Components/Header/styles.jsx
@@ -22,7 +22,7 @@ export const MenuButton = styled.button`
   border: 0;
   background-color: transparent;
   font-size: 16px;
-  font-weight: 500;
   cursor: pointer;
   color: #fff;
+  font-family: 'Jua';
 `;
