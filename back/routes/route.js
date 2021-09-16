9baa13ffb65a22f1287898aa09981ec6544377ab
wkjung0624 <>
2021-09-16 23:55:57 +0900

    Add root path('/') route

--- back/routes/route.js
+++ back/routes/route.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const controller = require('../controllers/controller')
 
+router.get('/', controller.welcome);
+
 router.get('/oauth/kakao', controller.getAuthenticate);
 router.get('/oauth/kakao/callback', controller.authCallBack);
 router.get('/oauth/kakao/logout', controller.logout);
@@ -9,9 +11,7 @@ router.get('/oauth/kakao/logout', controller.logout);
 router.get('/success', controller.success);
 router.get('/fail', controller.fail);
 
-router.get('/f1', controller.f1);
-router.get('/f2', controller.f2);
-
+router.get('/whoami', controller.whoami);
 
 router.get('/api/:alias', controller.getQuery);
 router.post('/api/:alias', controller.postQuery);
