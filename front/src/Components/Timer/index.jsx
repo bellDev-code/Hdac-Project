ad76c90567618ea863b414bf0f619fd726e3a85a
bellDev-code <sumaoo20@naver.com>
2021-09-17 14:20:26 +0900

    Add Timer

--- front/src/Components/Timer/index.jsx
+++ front/src/Components/Timer/index.jsx
@@ -1,36 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 
-const Timer = () => {
-  const [minutes, setMinutes] = useState(parseInt(59));
-  const [seconds, setSeconds] = useState(parseInt(59));
+const Timer = (hoursMinSecs) => {
+  const { days = 29, hours = 23, minutes = 59, seconds = 59 } = hoursMinSecs;
+  const [[drs, hrs, mins, secs], setTime] = useState([days, hours, minutes, seconds]);
+
+  const tick = () => {
+    if (drs === 0 && hrs === 0 && mins === 0 && secs === 0) reset();
+    else if (drs === 0 && hrs === 0) {
+      setTime([drs - 1, 23, 59, 59]);
+    } else if (mins === 0 && secs === 0) {
+      setTime([drs, hrs - 1, 59, 59]);
+    } else if (secs === 0) {
+      setTime([drs, hrs, mins - 1, 59]);
+    } else {
+      setTime([drs, hrs, mins, secs - 1]);
+    }
+  };
+
+  const reset = () => setTime([parseInt(days), parseInt(hours), parseInt(minutes), parseInt(seconds)]);
 
   useEffect(() => {
-    const countdown = setInterval(() => {
-      if (parseInt(seconds) > 0) {
-        setSeconds(parseInt(seconds) - 1);
-      }
-
-      if (parseInt(seconds) === 0) {
-        if (parseInt(minutes) === 0) {
-          clearInterval(countdown);
-        } else {
-          setMinutes(parseInt(minutes) - 1);
-          setSeconds(59);
-        }
-      }
-    }, 1000);
-    return () => clearInterval(countdown);
-  }, [minutes, seconds]);
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
 
   const TimeArea = styled.div`
-    font-size: 30px;
+    font-size: 35px;
     padding-bottom: 30px;
   `;
 
   return (
     <TimeArea>
-      {minutes}분 {seconds < 10 ? `0${seconds}` : seconds}초
+      {`${drs.toString().padStart(2, '0')}일 ${hrs.toString().padStart(2, '0')}시간 ${mins
+        .toString()
+        .padStart(2, '0')}분${secs.toString().padStart(2, '0')}초`}
     </TimeArea>
   );
 };
