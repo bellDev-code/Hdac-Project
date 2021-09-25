433d49ecdd5cde8b8da50a8af337fa597cfd156b
wkjung0624 <>
2021-09-25 20:48:25 +0900

    Add function for market order to redis

--- back/controllers/controller.js
+++ back/controllers/controller.js
@@ -1,22 +1,13 @@
 const passport = require("../models/passport");
 const mariaDB = require("../models/mariadb");
+const redis = require("../models/redis");
 
 module.exports = {
-  welcome: {},
-  getAuthenticate: passport.authenticate("kakao"),
-  authCallBack: passport.authenticate("kakao", {
-    successRedirect: "/success",
-    failureRedirect: "/fail",
-  }),
-  logout: (req, res) => {
-    req.logout();
-    req.session.destroy(function (err) {
-      res.redirect("/");
-    });
-  },
+
   success: (req, res) => {
     res.send("success");
   },
+
   fail: (req, res) => {
     res.send("fail");
   },
@@ -26,9 +17,22 @@ module.exports = {
     res.send(req.user);
   },
 
-  getQuery: async (req, res) => {
+  marketOrder: (req, res) => {
+    console.log(req.body);
+
+    redis(
+      req.body.nftID,
+      req.body.orderType,
+      req.body.orderer,
+      req.body.amount,
+      req.body.volume);
+
+    res.send(req.header('Referer'));
+  },
+  postQuery: async (req, res) => {
     try {
-      const queryResult = await mariaDB.sendQuery(req.params.alias);
+      console.log(req.body);
+      const queryResult = await mariaDB.sendQuery(req.params.alias, req.body.param, req.body.where);
       res.send(JSON.stringify(queryResult));
     } catch (err) {
       res.status(500).send({
@@ -40,25 +44,55 @@ module.exports = {
     }
   },
 
-  postQuery: async (req, res) => {
-    try {
-      await mariaDB.sendQuery(req.params.alias);
-      if (req.body.param.length > 0) {
-        for (let key in req.body.param[0])
-          req.session[key] = req.body.param[0][key];
-        res.send(req.body.param[0]);
-      } else {
-        res.send({
-          error: "Please try again or contact system manager.",
-        });
-      }
-    } catch (err) {
-      res.send({
-        error: "DB access error",
-      });
-    }
+  // postQuery: async (req, res) => {
+  //   try {
+  //     await mariaDB.sendQuery(req.params.alias);
+  //     if (req.body.param.length > 0) {
+  //       for (let key in req.body.param[0])
+  //         req.session[key] = req.body.param[0][key];
+  //       res.send(req.body.param[0]);
+  //     } else {
+  //       res.send({
+  //         error: "Please try again or contact system manager.",
+  //       });
+  //     }
+  //   } catch (err) {
+  //     res.send({
+  //       error: "DB access error",
+  //     });
+  //   }
+  // },
+
+  welcome: (req, res) => {
+    res.send("test");
   },
-  welcome: () => {},
-  getAuthenticate: () => {},
-  authCallBack: () => {},
+
+  getAuthenticate: passport.authenticate("kakao"),
+
+  authCallBack: passport.authenticate("kakao", {
+    successRedirect: "http://127.0.0.1:3080/",
+    failureRedirect: "http://127.0.0.1/fail",
+  }),
+
+  register: async (req, res) => {
+    var log = await mariaDB.sendQuery('signUp', [req.body.id, req.body.passwd]);
+    res.send(log);
+  },
+
+  locallogin: async (req, res) => {
+    var log = await mariaDB.sendQuery('getUserAccount', [req.body.id, req.body.passwd]);
+    res.send(log);
+  },
+
+  login: async (req, res) => {
+    var log = await mariaDB.sendQuery('getUserAccount', [req.body.id, req.body.passwd]);
+    res.send(log);
+  },
+
+  logout: (req, res) => {
+    req.logout();
+    req.session.destroy(function (err) {
+      res.redirect("/");
+    });
+  },
 };
